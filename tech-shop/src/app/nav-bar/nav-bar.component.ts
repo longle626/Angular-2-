d21b978760f61e8ffe96e75b103abee8c3ed88b8
longle626 <lenguyenbaolong@yahoo.com>
2018-08-02 20:26:14 -0700

    authen
    implement login/logout with google authenticate

--- tech-shop/src/app/nav-bar/nav-bar.component.ts
+++ tech-shop/src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable'
+
+
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
+	user$ : Observable <firebase.User>;
 
-  constructor() { }
+  constructor( private afAuth : AngularFireAuth) { 
+  	this.user$ = afAuth.authState;
 
-  ngOnInit() {
   }
 
+  logout() {
+  	this.afAuth.auth.signOut();
+
+  }
 }
