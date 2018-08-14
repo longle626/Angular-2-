0f7578b97a849da554871c1b0111eff42531a452
longle626 <lenguyenbaolong@yahoo.com>
2018-08-13 20:41:48 -0700

    added preview product , navigated to product list page after save product

--- tech-shop/src/app/admin/product-form/product-form.component.ts
+++ tech-shop/src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from './../../category.service';
 import { ProductsService } from './../../products.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -13,8 +13,10 @@ export class ProductFormComponent implements OnInit {
 	categories$;
 
   constructor(
-  	catergoryService: CategoryService,
-  	private productsService: ProductsService){ 
+  	private router: Router,
+  	private catergoryService: CategoryService,
+  	private productsService: ProductsService )
+  	{ 
   		this.categories$ = catergoryService.getCategories();
   	}
 
@@ -23,6 +25,6 @@ export class ProductFormComponent implements OnInit {
   // send new products to products service
   save(product){
   	this.productsService.create(product);
-
+  	this.router.navigate(['/admin/products']);
   }
 }
