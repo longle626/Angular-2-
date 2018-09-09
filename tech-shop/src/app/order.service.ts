590bb509446778262f4871028fd4d108bec64b57
longle626 <lenguyenbaolong@yahoo.com>
2018-09-08 21:33:35 -0700

    Clear shopping cart after order placed.

--- tech-shop/src/app/order.service.ts
+++ tech-shop/src/app/order.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { ShoppingCartService } from './shopping-cart.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private db : AngularFireDatabase) { }
-  // Store order's information into firebase
-  storeOrder(order : object){
-  	return this.db.list('/order/').push(order);	
+  constructor(
+  	private db : AngularFireDatabase,
+  	private shoppingCartService: ShoppingCartService,
+  ) { }
+  // Store order's information into firebase 
+  // and removed all items in shopping cart.
+  async storeOrder(order : object){
+  	let orders = await this.db.list('/order/').push(order);	
+  	this.shoppingCartService.clearCart();
+  	return orders;
   }
 }
